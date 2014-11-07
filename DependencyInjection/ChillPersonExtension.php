395d9ef67b457fb4c11275d20d87b9fd63792818
Marc Ducobu <marc@champs-libres.coop>
2014-11-07 19:16:47 +0100

    Define Person as Customisable class (Custom Field)

--- DependencyInjection/ChillPersonExtension.php
+++ DependencyInjection/ChillPersonExtension.php
@@ -29,8 +29,24 @@ class ChillPersonExtension extends Extension implements PrependExtensionInterfac
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
+
+    private function declarePersonAsCustomizable (ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+        if (!isset($bundles['ChillCustomFieldsBundle'])) {
+            throw new MissingBundleException('ChillCustomFieldsBundle');
+        }
+
+        $container->prependExtensionConfig('chill_custom_fields',
+            array('customizables_entities' => 
+                array(
+                    array('class' => 'Chill\PersonBundle\Entity\Person', 'name' => 'PersonEntity')
+                )
+            )
+        );
+    }
     
-        public function prepend(ContainerBuilder $container) 
+    public function prepend(ContainerBuilder $container) 
     {
         $bundles = $container->getParameter('kernel.bundles');
         //add ChillMain to assetic-enabled bundles
@@ -42,5 +58,7 @@ class ChillPersonExtension extends Extension implements PrependExtensionInterfac
         $asseticConfig['bundles'][] = 'ChillPersonBundle';
         $container->prependExtensionConfig('assetic', 
                 array('bundles' => array('ChillPersonBundle')));
+
+        $this-> declarePersonAsCustomizable($container);
     }
 }

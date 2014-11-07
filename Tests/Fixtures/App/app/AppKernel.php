ec689f4a3f381693ce91807e264d9b8b69616f9a
Julien Fastré <julien@fastre.info>
2014-11-08 00:51:19 +0100

    add app fixture

---
+++ Tests/Fixtures/App/app/AppKernel.php
@@ -0,0 +1,43 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Chill\CustomFieldsBundle\ChillCustomFieldsBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Chill\PersonBundle\ChillPersonBundle(),
+            #add here all the required bundle (some bundle are not required)
+        );
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return sys_get_temp_dir().'/PersonBundle/cache';
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogDir()
+    {
+        return $this->getRootDir().'/../logs';
+    }
+}
+

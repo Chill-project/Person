8127ea8d49e094c81ce65e838416953bc8d47678
Julien Fastré <julien.fastre@champs-libres.coop>
2015-06-08 00:33:39 +0200

    add first impl of PersonVoter (not tested) [ci skip]

---
+++ Security/Authorization/PersonVoter.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * Copyright (C) 2015 Julien Fastré <julien.fastre@champs-libres.coop>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Chill\PersonBundle\Security\Authorization;
+
+use Chill\MainBundle\Security\Authorization\AbstractChillVoter;
+use Chill\MainBundle\Entity\User;
+use Chill\MainBundle\Security\Authorization\AuthorizationHelper;
+
+/**
+ * 
+ *
+ * @author Julien Fastré <julien.fastre@champs-libres.coop>
+ */
+class PersonVoter extends AbstractChillVoter
+{
+    const CREATE = 'CHILL_PERSON_CREATE';
+    const UPDATE = 'CHILL_PERSON_UPDATE';
+    
+    /**
+     *
+     * @var AuthorizationHelper
+     */
+    protected $helper;
+    
+    public function __construct(AuthorizationHelper $helper)
+    {
+        $this->$helper = $helper;
+    }
+    
+    protected function getSupportedAttributes()
+    {
+        return array(self::CREATE, self::UPDATE);
+    }
+
+    protected function getSupportedClasses()
+    {
+        return array('Chill\PersonBundle\Entity\Person');
+    }
+
+    protected function isGranted($attribute, $person, $user = null)
+    {
+        if (!$user instanceof User) {
+            return false;
+        }
+        
+        if ($this->helper->userCanReachCenter($user, $person->getCenter())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

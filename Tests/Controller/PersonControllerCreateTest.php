e8ca4ce4842e619342a90d4343b725c2946f7d70
Julien Fastré <julien.fastre@champs-libres.coop>
2015-06-19 17:31:27 +0200

    should fix reference problem

--- Tests/Controller/PersonControllerCreateTest.php
+++ Tests/Controller/PersonControllerCreateTest.php
@@ -201,7 +201,8 @@ class PersonControllerCreateTest extends WebTestCase
         $this->assertTrue($form->has(self::CENTER_INPUT),
               'The page contains a "center" input');
         $centerInput = $form->get(self::CENTER_INPUT);
-        $lastCenterInputValue = end($centerInput->availableOptionValues());
+        $availableValues = $centerInput->availableOptionValues();
+        $lastCenterInputValue = end($availableValues);
         $centerInput->setValue($lastCenterInputValue);
         
         $client->submit($form);
